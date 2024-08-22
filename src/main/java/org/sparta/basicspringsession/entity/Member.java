df35fbc4e4c782cdf0760211cdf8d0a5a9e6fda6
leejaehee0807 <leejaehee0807@naver.com>
2024-08-22 15:03:20 +0900

    CRUD 구현

--- src/main/java/org/sparta/basicspringsession/entity/Member.java
+++ src/main/java/org/sparta/basicspringsession/entity/Member.java
@@ -11,9 +11,17 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Member {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-
+    @Id @GeneratedValue(strategy =GenerationType.IDENTITY )
     private Long id;
     private String name;
+
+    public Member(String name){
+        this.name =name;
+    }
+
+    public void update(String name){
+        this.name = name;
+    }
+
+
 }
